dac7fae5ddc122878253b5cd96213c20eb30f6be
snissy <nisse.sb@gmail.com>
2022-03-17 16:49:24 +0100

    finalSubmission

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import itertools
 import time
 
 import numpy as np
 import pandas as pd
+import tqdm
 from matplotlib import pyplot as plt
 import seaborn as sns
 from sklearn.decomposition import PCA
@@ -16,16 +18,22 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from scipy import stats
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import scale, MinMaxScaler
 
 # trainingPath = "data/TrainOnMe_RE.csv"
 # evaluationPath = "data/EvaluateOnMe_RE.csv"
-trainingPath = "data/TrainOnMe-2.csv"
-evaluationPath = "data/EvaluateOnMe-2.csv"
-# trainingPath = "data/eval3.csv"
-# evaluationPath = "data/train3.csv"
 
-config = {"outliersZValue": 3, "corrDropLimit": 0.95}
+# Most testing has been done on this .
+# trainingPath = "data/TrainOnMe-2.csv"
+# evaluationPath = "data/EvaluateOnMe-2.csv"
+
+trainingPath = "data/train3.csv"
+evaluationPath = "data/eval3.csv"
+
+trainingPath = "data/TrainOnMe-4.csv"
+evaluationPath = "data/EvaluateOnMe-4.csv"
+
+config = {"nComponents": 22}
 
 
 def warn(*args, **kwargs):
@@ -70,22 +78,9 @@ def removeNoise(trainingData, evaluationData):
         else:
             if evaluationData[cName].dtype.name == 'object':
 
-                # TODO I think all this is needed when doing the real assignment. Seems that I got a really dirty dataset.
-                cleanEvalData = [v.replace(".", "").replace(chr(8722), chr(45)) for v in evaluationData.get(cName)]
-                quantitativeColum = all(is_float(element) for element in cleanEvalData)
-                # fix this.
-                if quantitativeColum:
-                    # we convert string to float
-                    #  This change.
-                    cleanTrainData = [v.replace(".", "").replace(chr(8722), chr(45)) for v in trainingData.get(cName)]
-                    trainingData[cName] = pd.to_numeric(cleanTrainData, downcast="float")
-                    evaluationData[cName] = pd.to_numeric(cleanEvalData, downcast="float")
-
-                else:
-                    # We remove rows with values that have categories that doesn't exist in the evaluation set.
-                    trainingData = trainingData[trainingData[cName].isin(uniqueValues)]
-                    trainingData[cName].replace(uniqueValues, list(range(len(uniqueValues))), inplace=True)
-                    evaluationData[cName].replace(uniqueValues, list(range(len(uniqueValues))), inplace=True)
+                trainingData = trainingData[trainingData[cName].isin(uniqueValues)]
+                trainingData[cName].replace(uniqueValues, list(range(len(uniqueValues))), inplace=True)
+                evaluationData[cName].replace(uniqueValues, list(range(len(uniqueValues))), inplace=True)
 
             elif evaluationData[cName].dtype.name == 'bool':
                 # trainingData = trainingData[trainingData[cName].isin(["False", 'True', 'TRUE', 'FALSE'])]
@@ -94,11 +89,7 @@ def removeNoise(trainingData, evaluationData):
                 trainingData[cName] = trainingData[cName].astype(int)
                 evaluationData[cName] = evaluationData[cName].astype(int)
 
-    # evaluationData.replace(["False", 'True'], [0, 1], inplace=True)
-    # trainingData.replace(["False", 'True'], [0, 1], inplace=True)
-    # outliers
-
-    trainingData = trainingData[(np.abs(stats.zscore(trainingData.drop('y', axis=1))) < 5).all(axis=1)]
+    trainingData = trainingData[(np.abs(stats.zscore(trainingData.drop('y', axis=1))) < 4).all(axis=1)]
     # TODO CHECK 3 or 4
 
     # Here we do correlation values. We should plot it first in order go get som insights.  to data
@@ -106,85 +97,38 @@ def removeNoise(trainingData, evaluationData):
     corrMatrix = trainingData.corr().abs()
     upper = corrMatrix.where(np.triu(np.ones(corrMatrix.shape), k=1).astype(bool))
 
-    dropLimit = 0.9
+    dropLimit = 0.90
     to_drop = [column for column in upper.columns if any(upper[column] >= dropLimit)]
     # Drop features
     trainingData.drop(to_drop, axis=1, inplace=True)
     evaluationData.drop(to_drop, axis=1, inplace=True)
-
-    for cName in evaluationData.columns:
-        if evaluationData[cName].dtype.name != 'bool':
-            dataMean = evaluationData[cName].mean()
-            dataStd = evaluationData[cName].std()
-            evaluationData[cName] = (evaluationData[cName] - dataMean) / dataStd
-            trainingData[cName] = (trainingData[cName] - dataMean) / dataStd
-
+    trainingData = trainingData.groupby('y').apply(lambda x: x.sample(frac=0.25))  # 0.015  # 0.15 took 100 minutes
     X_train = trainingData.drop('y', 1)
     Y_train = trainingData['y']
+    scaling = MinMaxScaler(feature_range=(-1, 1)).fit(X_train)
+    X_train = scaling.transform(X_train)
+    evaluationData = scaling.transform(evaluationData)
 
-    pca = PCA()
-    pca.fit(evaluationData)
-    newNumber = len(evaluationData.columns) - sum(
-        1 for cV in pca.explained_variance_ratio_ if cV <= (1 / float(len(evaluationData.columns))) * 0.65)
-    print((1 / float(len(evaluationData.columns))) * 0.65)
-    # 0.05 was really good
-
-    pca = PCA(n_components=newNumber)
+    pca = PCA(n_components=18).fit(X_train)
     # # These will become Numpy arrays
-    X_train = pca.fit_transform(X_train)
-    evaluationData = pca.fit_transform(evaluationData)
-
-    # hm = sns.heatmap(corrMatrix, annot=True)
-    # hm.set(title="Correlation matrix of IRIS data\n")
-    # plt.show()
-    # Mean normalize data.
-    # mean normalization
-
-    # pca = PCA(n_components=X.shape[1])
-    # pca.fit(X)
-    # print(pca.explained_variance_ratio_)
-    # TODO use sklearn SCALE FUNCTION, StandardScaler
-
-    # for cName in evaluationData.columns:
-    #     if evaluationData[cName].dtype.name != 'bool':
-    #         dataMax = evaluationData[cName].max()
-    #         dataMin = evaluationData[cName].min()
-    #         evaluationData[cName] = (evaluationData[cName] - dataMin) / (dataMax - dataMin)
-    #         trainingData[cName] = (trainingData[cName] - dataMin) / (dataMax - dataMin)
+    X_train = pca.transform(X_train)
+    evaluationData = pca.transform(evaluationData)
 
     return X_train, Y_train, evaluationData
-
-
-def splitData(data, splitvalue=0.75):
-    """
-    :param data:
-    :param splitvalue:
-    :return: training set and validation set.
-    """
-    return train_test_split(data, test_size=1 - splitvalue)
 
 
 if __name__ == '__main__':
 
     X_train, Y_train, X_eval = removeNoise(readData(trainingPath), readData(evaluationPath))
-
-    # le = preprocessing.LabelEncoder()
-    # le.fit(trainTargets)
-    # trainTargets = le.transform(trainTargets)
-
-    # Now we can start classify the data
-
     print("Number of points to train on is: {}".format(len(X_train)))
-    # The best parameters are {'C': 4.259662151757895, 'gamma': 0.0383646083799769} with a score of 0.62060
     classifiers = [
         KNeighborsClassifier(15),
         SVC(kernel="linear", C=0.025),
-        SVC(kernel="rbf", C=4.259662151757895, gamma=0.0383646083799769),
-        # GaussianProcessClassifier(1.0 * RBF(1.0)),
+        SVC(kernel="rbf", C=1.503060, gamma='auto'),
         GradientBoostingClassifier(),  # SLOW
         DecisionTreeClassifier(),
         RandomForestClassifier(criterion='entropy', n_estimators=250, max_features='sqrt'),
-        MLPClassifier(alpha=1.4446930579460697, max_iter=1000),
+        MLPClassifier(alpha=0.1778439837548613, max_iter=1000),
         AdaBoostClassifier(),
         AdaBoostClassifier(GaussianNB()),
         GaussianNB(),
@@ -193,11 +137,9 @@ if __name__ == '__main__':
     ]
 
 
-    # trainData, validationData = splitData(trainData)
-
-    # sparse coordinate arrays
-
     def gridSearch():
+
+        print("Staring grid search")
 
         def svc():
             startTime = time.time()
@@ -205,11 +147,11 @@ if __name__ == '__main__':
             dC = 5
             sG = 0.00001
             dG = 2.5
-            C_range = np.linspace(sC, sC + dC, 150)
-            gamma_range = np.linspace(sG, sG + dG, 150)
-            param_grid = dict(gamma=gamma_range, C=C_range)
-            cv = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=42)
-            grid = GridSearchCV(SVC(), param_grid=param_grid, cv=cv)
+            C_range = np.linspace(1.593541468827725 - 0.15, 1.593541468827725 + 0.15, 500)
+            gamma_range = np.linspace(sG, sG + dG, 100)
+            param_grid = dict(gamma=['auto'], C=[1.503060], kernel=['linear', 'poly', 'rbf', 'sigmoid'])
+            cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+            grid = GridSearchCV(SVC(), param_grid=param_grid, cv=cv, n_jobs=14, verbose=10)
             grid.fit(X_train, Y_train)
 
             print(
@@ -222,12 +164,10 @@ if __name__ == '__main__':
         def mlp():
             startTime = time.time()
 
-            aStart = 1.4446930579460697
-            aD = abs(1.6428571428571426 - 1.4446930579460697)*1.1
-            a_range = np.linspace(aStart-aD, aStart+aD, 250)
+            a_range = np.linspace(0, 0.19684194472866112 + 1, 250)
             param_grid = dict(alpha=a_range)
-            cv = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=42)
-            grid = GridSearchCV(MLPClassifier(), param_grid=param_grid, cv=cv)
+            cv = StratifiedShuffleSplit(n_splits=6, test_size=0.2, random_state=42)
+            grid = GridSearchCV(MLPClassifier(), param_grid=param_grid, cv=cv, n_jobs=14, verbose=10)
             grid.fit(X_train, Y_train)
 
             print(
@@ -237,32 +177,110 @@ if __name__ == '__main__':
 
             print("Tiden var {}".format(time.time() - startTime))
 
-        mlp()
+        def stacking():
+            forStacking = [
+                ('knn', KNeighborsClassifier(20)),
+                ('Lsvc', SVC(kernel="linear", C=1)),
+                ('rbf', SVC(kernel="rbf", C=1.503060, gamma='auto')),
+                ('gbc', GradientBoostingClassifier()),
+                ('dtt', DecisionTreeClassifier()),
+                ('rfc', RandomForestClassifier(criterion='entropy', n_estimators=250, max_features='sqrt')),
+                ('mlp', MLPClassifier(alpha=0.1778439837548613, max_iter=1000)),
+                ('ada', AdaBoostClassifier()),
+                ('naiveGauss', GaussianNB()),
+                ('qda', QuadraticDiscriminantAnalysis(),),
+                ('lda', LinearDiscriminantAnalysis())
+            ]
+            combos = []
+            finalRes = (-10, None, None)
+            for i in range(3, 5):
+                t = itertools.combinations(forStacking, i)
+                for c in t:
+                    combos.append(list(c))
+
+            for c in tqdm.tqdm(combos):
+                clf = StackingClassifier(estimators=c, final_estimator=MLPClassifier(max_iter=1000), cv=7)
+                result = cross_val_score(clf, X_train, Y_train, cv=16, n_jobs=16)
+                finalRes = max((np.mean(result), list(c)), finalRes, key=lambda x: x[0])
+
+            print(finalRes)
+            cache = open("finalRes.txt", mode="w")
+            cache.write(str(finalRes))
+            cache.close()
+
+        # svc()
+        # mlp()
+        # stacking()
 
 
     def testFunction():
 
-        sumCache = []
-
-        # print("nCV: {}\t\tMean score: {}\tStd: {}\t\tClassifier: {}\t\t\t".format(n,round(np.mean(result), 4), round(np.std(result), 4), clf.__class__.__name__, ))
-
-        n = 10
-
+        n = 14
+        finalRes = (-10, "none")
         for clf in classifiers:
-            result = cross_val_score(clf, X_train, Y_train, cv=n)
+            result = cross_val_score(clf, X_train, Y_train, cv=n, n_jobs=14)  # verbose=5,
             print("nCV: {}\t\tMean score: {}\tStd: {}\t\tClassifier: {}\t\t\t".format(n, round(np.mean(result), 4),
                                                                                       round(np.std(result), 4),
                                                                                       clf.__class__.__name__, ))
+            finalRes = max((np.mean(result), clf.__class__.__name__,), finalRes, key=lambda x: x[0])
 
-        estimators = [('svc', SVC()),
-                      ('mlp', MLPClassifier(alpha=0.6, max_iter=1000)),
-                      ('qda', QuadraticDiscriminantAnalysis())]
+        # estimators = [('knn', KNeighborsClassifier(n_neighbors=20)), ('Lsvc', SVC(C=1, kernel='linear')), ('mlp', MLPClassifier(alpha=0.1778439837548613, max_iter=1000)), ('naiveGauss', GaussianNB())]
+        estimators = [('knn', KNeighborsClassifier(n_neighbors=20)), ('Lsvc', SVC(C=1, kernel='linear')),
+                      ('mlp', MLPClassifier(alpha=0.1778439837548613, max_iter=1000)), ('ada', AdaBoostClassifier())]
 
-        clf = StackingClassifier(estimators=estimators, final_estimator=LogisticRegression(), cv=10)
-        result = cross_val_score(clf, X_train, Y_train, cv=n)
+        clf = StackingClassifier(estimators=estimators, final_estimator=MLPClassifier(max_iter=1250), cv=7)
+
+        result = cross_val_score(clf, X_train, Y_train, cv=n, n_jobs=14, verbose=10)
         print("nCV: {}\t\tMean score: {}\tStd: {}\t\tClassifier: {}\t\t\t".format(n, round(np.mean(result), 4),
                                                                                   round(np.std(result), 4),
                                                                                   clf.__class__.__name__, ))
 
-    #gridSearch()
-    testFunction()
+        finalRes = max((np.mean(result), clf.__class__.__name__,), finalRes, key=lambda x: x[0])
+        print(finalRes)
+        print("Evaluation result")
+
+        clf.fit(X_train, Y_train)
+
+        evalRes = clf.predict(X_eval, )
+        print(*evalRes)
+
+        cache = open("evalRes.txt", mode="w")
+
+        for label in evalRes:
+            cache.write(label + "\n")
+        cache.close()
+
+        return finalRes
+
+
+    # gridSearch()
+
+    # finalTest()
+
+    # print("Done!")
+
+
+    def finalSubmission():
+
+        estimators = [('knn', KNeighborsClassifier(n_neighbors=20)), ('Lsvc', SVC(C=1, kernel='linear')),
+                      ('mlp', MLPClassifier(alpha=0.1778439837548613, max_iter=1000)), ('ada', AdaBoostClassifier())]
+
+        clf = StackingClassifier(estimators=estimators,
+                                 final_estimator=MLPClassifier(max_iter=1250),
+                                 cv=14,
+                                 n_jobs=14,
+                                 verbose=10)
+
+        clf.fit(X_train, Y_train)
+
+        evalRes = clf.predict(X_eval)
+
+        cache = open("evalRes3.txt", mode="w")
+
+        for label in evalRes:
+            cache.write(label + "\n")
+            print(label)
+        cache.close()
+
+    finalSubmission()
+
